5499105fb764432d799101529d6c0a329c3f05a6
apocryptex <ankit7166428@gmail.com>
2026-04-24 13:46:37 +0530

    Add Prisma schema for university managment system

---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import { NestFactory } from "@nestjs/core";
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from "@nestjs/platform-fastify";
+import "reflect-metadata";
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+
+  app.setGlobalPrefix("api");
+  app.enableCors();
+
+  await app.listen(8000, "0.0.0.0");
+}
+
+bootstrap();
