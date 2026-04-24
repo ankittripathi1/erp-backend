ddfd0a6ee09281c4326b813beff000236dae7314
apocryptex <ankit7166428@gmail.com>
2026-04-24 13:54:41 +0530

    Remove lagacy Django backend

--- apps/placements/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class PlacementsConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "apps.placements"
-    label = "placements"
