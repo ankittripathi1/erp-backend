312fe855acf2f5b5514f1e7d354462a91dc3a7f0
apocryptex <ankit7166428@gmail.com>
2026-04-25 13:49:37 +0530

    Serialize BigInt values in API response

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,19 @@ import { AppController } from "./app.controller";
 import { PrismaService } from "./prisma/prisma.service";
 import { UserModule } from "./modules/users/users.module";
 import { RolesModule } from "./modules/roles/roles.module";
+import { APP_INTERCEPTOR } from "@nestjs/core";
+import { BigIntSerializerInterceptor } from "./common/interceptors/bigint-serializer.interceptor";
 
 @Module({
   imports: [ConfigModule.forRoot({ isGlobal: true }), UserModule, RolesModule],
   controllers: [AppController],
-  providers: [PrismaService],
+  providers: [
+    PrismaService,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: BigIntSerializerInterceptor,
+    },
+  ],
   exports: [PrismaService],
 })
 export class AppModule {}
